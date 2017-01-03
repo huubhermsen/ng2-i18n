02bf5ba57edffcd44b203a3d4eb38e856c94eec4
Huub Hermsen <huub.hermsen@gmail.com>
2017-01-04 00:43:28 +0100

    Translate service dependencies and config

--- src/translate.service.ts
+++ src/translate.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from "@angular/core";
+import { Injectable, Optional } from "@angular/core";
+import { Observable } from "rxjs/Observable";
+
+const MessageFormat = require("messageformat");
+
+class TranslateServiceConfig
+{
+  locale: string;
+  hot: boolean = false;
+}
 
 @Injectable()
-export class TranslateService {}
+class TranslateService
+{
+  private _locale: string;
+  private _hot: boolean;
+  
+  constructor(@Optional() config: TranslateServiceConfig)
+  {
+    if (config) this._setConfig(config);
+  }
+
+  private _setConfig(config: TranslateServiceConfig)
+  {
+    this._locale = config.locale;
+    this._hot = config.hot;
+  }
+}
+
+export {
+  TranslateService
+}
